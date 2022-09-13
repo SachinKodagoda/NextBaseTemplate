8f7755a16a0111e751dc8cce69c0470c25ef4c22
Duminda Kodagoda <duminda.g.k@gmail.com>
2022-09-14 00:01:48 +0530

    clean ups

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,4 @@ function Home(): JSX.Element {
 
 export default Home;
 
-const Container = styled.div`
-  width: 100%;
-  margin: auto;
-  height: calc(100vh - 60px);
-  display: flex;
-  flex-direction: column;
-`;
+const Container = styled.div``;
