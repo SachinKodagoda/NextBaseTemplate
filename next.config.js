d78b0f8e398cbeae840e0056a4360f154e33e16c
Duminda Kodagoda <duminda.g.k@gmail.com>
2022-09-23 14:01:42 +0530

    few updates for the template

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
+  compiler: {
+    styledComponents: true,
+    swcMinify: true,
+  },
   images: {
     domains: ['google.com'],
   },
